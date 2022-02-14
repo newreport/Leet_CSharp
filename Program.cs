988765c11a80155b2db2efdd84c1a44df2d8ea16
newreport <haruki959@qq.com>
2022-02-14 22:18:57 +0800

    冒泡排序

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("123");
+﻿
+
+using Leet;
+
+Sort();
+
+void Sort()
+{
+
+    Sort sort = new();
+    List<int> ls = new List<int>();
+    Random rd = new Random();
+    for (int i = 0; i < 10; i++)
+    {
+        ls.Add(rd.Next(0, 9999));
+    }
+    //冒泡
+    sort.Bubble(ls.ToArray());
+
+
+}
---
+++ Sort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet
+{
+    public class Sort
+    {
+        /// <summary>
+        /// 冒泡排序
+        /// </summary>
+        public void Bubble(int[] arr)
+        {
+            int temp = 0;
+            if (arr.Length > 1)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    for (int j = 0; j < arr.Length - i-1; j++)
+                    {
+                        if (arr[j] > arr[j + 1])
+                        {
+                            temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(",", arr));
+        }
+
+    }
+}
