282b3317b4217bad5b2fc375d2fc699c3cc905af
newreport <haruki959@qq.com>
2022-02-14 22:31:19 +0800

    选择排序

--- Program.cs
+++ Program.cs
@@ -10,12 +10,13 @@ void Sort()
     Sort sort = new();
     List<int> ls = new List<int>();
     Random rd = new Random();
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 100; i++)
     {
         ls.Add(rd.Next(0, 9999));
     }
     //冒泡
     sort.Bubble(ls.ToArray());
-
+    //选择
+    sort.Selection(ls.ToArray());
 
 }
--- Sort.cs
+++ Sort.cs
@@ -10,6 +10,9 @@ namespace Leet
     {
         /// <summary>
         /// 冒泡排序
+        /// 
+        /// 思想是交换相邻两项的值
+        /// 时间复杂度 O(n^2)
         /// </summary>
         public void Bubble(int[] arr)
         {
@@ -18,7 +21,7 @@ namespace Leet
             {
                 for (int i = 0; i < arr.Length - 1; i++)
                 {
-                    for (int j = 0; j < arr.Length - i-1; j++)
+                    for (int j = 0; j < arr.Length - i - 1; j++)
                     {
                         if (arr[j] > arr[j + 1])
                         {
@@ -32,5 +35,31 @@ namespace Leet
             Console.WriteLine(String.Join(",", arr));
         }
 
+        /// <summary>
+        /// 选择排序
+        /// 
+        /// 思想是选择最小(大)的值，并记录索引
+        /// 时间复杂度 O(n^2)
+        /// </summary>
+        /// <param name="arr"></param>
+        public void Selection(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int index = i;
+                int temp = arr[i];
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (temp > arr[j])
+                    {
+                        index = j;
+                        temp = arr[j];
+                    }
+                }
+                arr[index] = arr[i];
+                arr[i] = temp;
+            }
+            Console.WriteLine(String.Join(",", arr));
+        }
     }
 }
