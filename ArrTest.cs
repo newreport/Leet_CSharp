745d393d5065b1db698757fdac79aa372e829dfb
newreport <haruki959@qq.com>
2022-02-22 19:46:34 +0800

    两数之和

---
+++ ArrTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leet
+{
+    public class ArrTest
+    {
+
+        /// <summary>
+        /// 两数之和
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int[] arr = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(target - nums[i]))
+                {
+                    arr[0] = i;
+                    arr[1] = dic[target - nums[i]];
+                    return arr;
+                }
+                else if(!dic.ContainsKey(nums[i]))
+                {
+                    dic.Add(nums[i], i);
+                }
+
+            }
+            return arr;
+        }
+    }
+}
